c25a166bcefd28c28c4e6bb156d1f20d517164e9
viju <avijusanjai@gmail.com>
2024-07-30 11:47:09 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongo=require('mongoose');
-mongo.connect('mongodb+srv://pocopepe:LdD17HW6csr8qwOs@meh.s7ipmyl.mongodb.net/ToDo_App');
+mongo.connect(*add your own mongodb url*);
 
 const todo= mongo.model('ToDo', {title: String, description: String, completed: Boolean});
 
